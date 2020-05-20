3f0670bbbe37c672c570cb8f0ca472b1395bc7b9
Andrew <andrewcatinat@gmail.com>
2020-05-20 03:01:34 -0700

    Merge branch 'Jarod' into old-state

--- New Unity Project/Assets/Scripts/Stage.cs
+++ New Unity Project/Assets/Scripts/Stage.cs
@@ -10,32 +10,67 @@ public class Stage : MonoBehaviour
 
     [SerializeField]
     public Room room;
-    private bool[,] stage;
-    //private int sizeStage = 3;
+    public int numberLine = 4;
+    public int numberCol = 3;
+    private bool[,] labyrinth;
+    private Room[,] rooms;
+    
 
     void Start(){
         //Instantiate(player, new Vector2(0, 0), Quaternion.identity);
         
-        bool[,] stage = { 
+        bool[,] matrix = { 
             { true, true, true}, 
-            { true, false, true},
-            { true, false, true},
-            { true, true, true}
+            { false, true, false},
+            { true, true, false},
+            { false, true, true}
         };
-        
-        int numberLine = stage.GetLength(0);
-        int numberCol = stage.GetLength(1);
 
+        this.labyrinth = matrix;
+        this.rooms = new Room[numberLine, numberCol];
         for(int i = 0; i < numberLine; i++){
             for(int j = 0; j < numberCol; j++){
-                Debug.Log("i : " + i + " / j : " + j + " / true/false : " + stage[i,j]);
-                if(stage[i,j]){
-                  Instantiate(room, new Vector2(39 * i , 21 * j), Quaternion.identity);
-                }
+
+                if(this.labyrinth[i,j])
+                    InitRoom(i,j);
+
             }
         }
 
     }
 
+    private void InitRoom(int x, int y){
+        this.rooms[x,y] = Instantiate(room, new Vector3(39 * y, 21 * x), Quaternion.identity);
+        bool[] roomAround = RoomAround(x,y);
+        this.rooms[x,y].SetupDoor(roomAround);
+    }
 
+    private bool[] RoomAround(int x, int y){
+        
+        if(x < 0 || y < 0 || x > numberLine || y > numberCol){
+            Debug.LogWarning("Les paramètres d'entrée dépassent la limite du tableau.");
+            return null;
+        }
+
+        bool[] roomAround = new bool[4];
+        int xPlus = x + 1;
+        int xMinus = x - 1;
+        int yPlus = y + 1;
+        int yMinus = y - 1;
+
+        //Vérifie qu'après calcul on ne sort pas des limites du tableau               
+        if(xPlus < numberLine)
+            roomAround[0] = labyrinth[xPlus, y];
+
+        if(yPlus < numberCol)
+            roomAround[1] = labyrinth[x, yPlus];
+
+        if(xMinus >= 0)
+            roomAround[2] = labyrinth[xMinus, y];        
+
+        if(yMinus >= 0)
+            roomAround[3] = labyrinth[x, yMinus];
+    
+        return roomAround;       
+    }
 }
